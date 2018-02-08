4e4fe5f6d7da52956078ce7c4d3f0203c337138f
try46 <c011610353@edu.teu.ac.jp>
2018-02-08 17:46:24 +0900

    make .sh

---
+++ ubuntu/install_linuxbrew.sh
@@ -0,0 +1,6 @@
+sudo apt install linuxbrew-wrapper -y
+
+brew
+
+sudo apt-get install build-essential -y
+echo 'export PATH="/home/try/.linuxbrew/bin:$PATH"' >> ~/.bashrc
---
+++ ubuntu/nvim_install.sh
@@ -0,0 +1,12 @@
+sudo apt install software-properties-common
+
+sudo add-apt-repository ppa:neovim-ppa/unstable
+
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt install python3-dev python3-pip
+
+pip3 install -U pip3
+
