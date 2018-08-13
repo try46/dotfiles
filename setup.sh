e278c7ebc0b0755d9cd62a6c2cb961b7922e3067
try46 <c011610353@edu.teu.ac.jp>
2018-08-13 22:59:06 +0900

    add darwin script and pyenv setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,45 @@
+export PYVERSION2 = 2.7.15
+export PYVERSION3 = 3.7.0
+export RUBYVERSION = 2.5.0
+export GOVERSION = 1.10.3
+#Install for dotfiles
 mkdir -p ~/.config/nvim/color
 cp ./nvim/* ~/.config/nvim/
-cp ./.bash_profile ~/.bash_profile
 cp -f ./nvim/color/ ~/.config/nvim/
 cp ./.latex* ~/
+
+git config --global user.name try46
+git config --global user.email popuman61@gmail.com
+
+if [[ 'uname'=='Darwin' ]]; then
+	#statements
+	if [[ ! -e 'which zsh' ]]; then
+		#statements
+		brew install zsh
+	elif [[ ! -d '.zprezto' ]]; then
+			#statements
+		git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+		cp ./.zshrc ~/.zshrc
+	elif [[ ! -d 'which tmux' ]]; then
+		brew install tmux
+	fi
+# Install for Middleware mac
+fi
+
+#Install pyenv rbenv goenv
+
+if [[ ! -e 'which pyenv' ]]; then
+	git clone https://github.com/pyenv/pyenv.git
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+	echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+	source ~/.zshrc
+
+	pyenv install $PYVERSION3
+	pyenv install $PYVERSION2
+
+	pyenv global $PYVERSION3
+	pip install neovim
+fi
+
+
