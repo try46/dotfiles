9017d02b90a26473e07ad7cdbb89664c38ff77a3
vagrant <vagrant@vagrant.vm>
2020-06-03 15:33:09 +0000

    setup用のスクリプト追加

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,2 +1,10 @@
-eval $(python3 -c "import glob,os;print('declare -a array=(', ' '.join(list(map(lambda x: os.path.split(x)[1], glob.glob('/home/vagrant/.dotfiles/nvim/*.vim')))), ')')")
-for ((i = 0; i < ${#array[@]}; i++)) { ln -s ~/.dotfiles/nvim/${array[i]} ~/.config/nvim/${array[i]}; }
+if [ -e ~/.config/nvim ]; then
+   echo "Start NeoVim SetUP!!"
+else
+   mkdir ~/.config/nvim
+fi
+
+eval $(python3 -c "import glob,os;print('declare -a array=(', ' '.join(list(map(lambda x: os.path.split(x)[1], glob.glob('/home/vagrant/.dotfiles/nvim/*.vim')))), ')')") 
+for ((i = 0; i < ${#array[@]}; i++)) { 
+	ln -s ~/.dotfiles/nvim/${array[i]} ~/.config/nvim/${array[i]};
+}
