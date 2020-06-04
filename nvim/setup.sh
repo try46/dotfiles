03c510e366e1d2e54c1eb2f49268f433a7a10888
try46 <popuman61@gmail.com>
2020-06-04 21:15:29 +0900

    ㊗ vim-plugへの移行終了

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,10 +1,10 @@
 if [ -e ~/.config/nvim ]; then
-   echo "Start NeoVim SetUP!!"
+  echo "Start NeoVim SetUP!!"
 else
-   mkdir ~/.config/nvim
+  mkdir ~/.config/nvim
 fi
 
-eval $(python3 -c "import glob,os;print('declare -a array=(', ' '.join(list(map(lambda x: os.path.split(x)[1], glob.glob('/home/vagrant/.dotfiles/nvim/*.vim')))), ')')") 
+eval $(python3 -c "import glob,os;print('declare -a array=(', ' '.join(list(map(lambda x: os.path.split(x)[1], glob.glob('/Users/tkrmt/.dotfiles/nvim/plugin/*.vim')))), ')')") 
 for ((i = 0; i < ${#array[@]}; i++)) { 
-	ln -s ~/.dotfiles/nvim/${array[i]} ~/.config/nvim/${array[i]};
+  ln -s ~/.dotfiles/nvim/plugin/${array[i]} ~/.config/nvim/${array[i]};
 }
